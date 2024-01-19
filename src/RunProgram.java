53711c9323d24cc89666fe93d44a42e2d3d9c4ad
RHONT <rg915i@yandex.ru>
2024-01-19 21:12:21 +0300

    - поправил ReadMe.md.

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -3,7 +3,7 @@ import generatorarray.Generator;
 
 public class RunProgram {
     public static void main(String[] args) {
-        Generator generator =new Generator(10,5, AlgorithmsList.SPIRAL);
+        Generator generator =new Generator(5,5, AlgorithmsList.SPIRAL);
         generator.printArray();
         System.out.println("=".repeat(20));
         generator.changeAlgorithmGeneration(AlgorithmsList.CLASSIC_FILL);
