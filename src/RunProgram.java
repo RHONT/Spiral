b867b2dc8bc8e05087c9668257a882d2d942093d
Rhont <updownuper@gmail.com>
2024-01-19 14:49:31 +0300

    rename class RunProgramm -> RunProgram

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -1,7 +1,7 @@
 import algoritmConvertArrayImpl.SpiralAlgorithms;
 import generatorspriralarray.GeneratorSpiralArray;
 
-public class RunProgramm {
+public class RunProgram {
     public static void main(String[] args) {
         GeneratorSpiralArray generatorSpiralArray =new GeneratorSpiralArray(10,5, SpiralAlgorithms.DEFAULT);
         generatorSpiralArray.printArray();
