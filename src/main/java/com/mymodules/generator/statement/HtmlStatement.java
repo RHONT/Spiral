2c01731c9a1b3377b3229d4b699df5bd69cc8349
Rhont <updownuper@gmail.com>
2024-01-24 15:07:57 +0300

    Рефактор
    Поднял наверх метод value(int[][]) в абстрактный класс, для:
    - HtmlStatement
    - TextStatement

--- src/main/java/com/mymodules/generator/statement/HtmlStatement.java
+++ src/main/java/com/mymodules/generator/statement/HtmlStatement.java
@@ -1,17 +1,12 @@
 package com.mymodules.generator.statement;
 
-import java.util.Arrays;
 
 /**
  * Вывод массива для HTML страниц
  */
 public class HtmlStatement extends Statement {
     @Override
-    public String value(int[][] inputArray) {
-        sb.append("<BR>");
-        for (var element : inputArray) {
-            sb.append(Arrays.toString(element)).append("<BR>");
-        }
-        return sb.toString();
+    void setDelimiter() {
+        _delimiter="<BR>";
     }
 }
--- src/main/java/com/mymodules/generator/statement/Statement.java
+++ src/main/java/com/mymodules/generator/statement/Statement.java
@@ -1,10 +1,27 @@
 package com.mymodules.generator.statement;
 
+import java.util.Arrays;
+
 /**
  * Адаптация вывода массива для разных целей
  */
 public abstract class Statement {
-    private String head= "Массив сгенерирован:";
-    protected StringBuilder sb=new StringBuilder(head);
-    public abstract String value(int[][] inputArray);
+    protected String _head= "Массив сгенерирован:";
+    protected String _delimiter;
+    protected StringBuilder sb=new StringBuilder();
+
+    public Statement() {
+        setDelimiter();
+    }
+
+    public String value(int[][] inputArray) {
+        sb.append(_head);
+        sb.append(_delimiter);
+        for (var element : inputArray) {
+            sb.append(Arrays.toString(element)).append(_delimiter);
+        }
+        return sb.toString();
+    }
+
+    abstract void setDelimiter();
 }
